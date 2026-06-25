34a396d287e2ee496211c2bf62b43b7ed5a8aedb
Aitijya Sarker Atibo <aitijyasarker@gmail.com>
2026-06-25 17:04:06 +0600

    OCP Demonstration

--- Task 3/Main2.java
+++ Task 3/Main2.java
@@ -1,1 +1,40 @@
+package OCP_Task;
+
+interface Shape {
+    double calculateArea();
+}
+
+class Rectangle implements Shape {
+    public double length;
+    public double width;
+
+    @Override
+    public double calculateArea() {
+        return length * width;
+
+    
+    }
+
+
+}
+
+class Circle implements Shape {
+    public double radius;
+
+    @Override
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+}
+
+class AreaCalculator {
+    public double calculateTotalArea(Shape[] shapes) {
+        double area = 0;
+        for (Shape shape : shapes) {
+            area += shape.calculateArea();
+        }
+        return area;
+    }
+}
+
 
