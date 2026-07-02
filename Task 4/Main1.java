663fc514c88fa384bbabea7e80906f80761b435e
Aitijya Sarker Atibo <aitijyasarker@gmail.com>
2026-07-02 16:34:58 +0600

    Add TeamMember, Employee, and Contractor classes

--- Task 4/Main1.java
+++ Task 4/Main1.java
@@ -1,1 +1,53 @@
+interface BonusEligible {
+    void calculateBonus();
+}
 
+class TeamMember {
+    protected String name;
+
+    public TeamMember(String name) {
+        this.name = name;
+    }
+
+    public void displayInfo() {
+        System.out.println("Name: " + name);
+    }
+}
+
+class Employee extends TeamMember implements BonusEligible {
+
+    public Employee(String name) {
+        super(name);
+    }
+
+    @Override
+    public void calculateBonus() {
+        System.out.println("Calculating standard employee bonus...");
+    }
+}
+
+class Contractor extends TeamMember {
+
+    public Contractor(String name) {
+        super(name);
+    }
+
+    public void submitInvoice() {
+        System.out.println("Submitting contractor invoice...");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        Employee employee = new Employee("Rahim");
+        employee.displayInfo();
+        employee.calculateBonus();
+
+        System.out.println();
+
+        Contractor contractor = new Contractor("Karim");
+        contractor.displayInfo();
+        contractor.submitInvoice();
+    }
+}
